a0c2e5aa59760731364406b421a845b679699e74
Dharshini Baskar <dharshinibaskarv@gmail.com>
2026-01-04 21:02:58 +0000

    Remove build artifacts and add gitignore

--- build/ros2_hello_world/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dharshini/ros2_ws/src/ros2-aid-simple-hello-world-node-ROS2-Assignment1-main
-# Build directory: /home/dharshini/ros2_ws/src/ros2-aid-simple-hello-world-node-ROS2-Assignment1-main/build/ros2_hello_world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
