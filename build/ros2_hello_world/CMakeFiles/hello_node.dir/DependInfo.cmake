0a680db4b08a0df51a2551f235f2b91a5c01f42d
Dharshini Baskar <dharshinibaskarv@gmail.com>
2026-01-04 19:49:27 +0000

    ROS2 Hello World assignment submission

---
+++ build/ros2_hello_world/CMakeFiles/hello_node.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dharshini/ros2_ws/src/ros2-aid-simple-hello-world-node-ROS2-Assignment1-main/src/hello_node.cpp" "CMakeFiles/hello_node.dir/src/hello_node.cpp.o" "gcc" "CMakeFiles/hello_node.dir/src/hello_node.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/hello_node.cpp
+++ src/hello_node.cpp
@@ -8,24 +8,27 @@
 using namespace std::chrono_literals;
 
 /*
- * TODO: Create a Class named 'HelloNode' that inherits from rclcpp::Node.
- * Requirements:
- * 1. The constructor should name the node "hello_world_node".
- * 2. Create a timer that triggers every 1000ms (1 second).
- * 3. The timer callback should print "Hello, World!" using the RCLCPP_INFO macro.
+ * HelloNode class
  */
-
 class HelloNode : public rclcpp::Node
 {
 public:
   HelloNode()
   : Node("hello_world_node")
   {
-    // TODO: Initialize the timer here
+    // Create a timer that runs every 1 second
+    timer_ = this->create_wall_timer(
+      1000ms,
+      std::bind(&HelloNode::timer_callback, this)
+    );
   }
 
 private:
-  // TODO: Define the timer_callback function here
+  // Timer callback function
+  void timer_callback()
+  {
+    RCLCPP_INFO(this->get_logger(), "Hello, World!");
+  }
 
   rclcpp::TimerBase::SharedPtr timer_;
 };
